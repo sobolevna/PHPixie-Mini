e0be3a8272c2072b89baaeb3bc33a1eb92aebac7
sobolevna <sobolevna@sobolevna-W65-67SF>
2016-06-04 23:14:46 +0300

    Ещё тесты

---
+++ tests/PHPixie/Tests/Framework/ConfigurationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PHPixie\Tests\Framework;
+
+/**
+ * Description of ConfigurationTest
+ *
+ * @author sobolevna
+ */
+class ConfigurationTest extends \PHPixie\Test\Testcase{
+    public function setUp($param) {
+        
+    }
+}
